dfcc4982ef59100752d11365cf0a6ded87efef04
Adam <adam.kostop@gmail.com>
2021-05-26 03:38:07 +0300

    initial commit

---
+++ MvcMusicStore/MvcMusicStore/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MvcMusicStore.Filters;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer(
+            new MvcMusicStore.Models.SampleData());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        //Used to handle errors thrown for RegisterGlobalFilters, RegisterRoutes
+        //>>
+        private void RegisterRoutes(RouteCollection routes)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            throw new NotImplementedException();
+        }
+        //<<
+
+        //Alternative:
+        //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        //RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+    //Represents Entity Framework database context
+    //Handle CRUD operations   
+
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
